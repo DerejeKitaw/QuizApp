99e14ca1774f39346212b0c9c868aed5a9d32dd4
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-05-17 23:15:59 -0400

    omponents generated

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 ```bash
 ng new QuizApp -sd client  --style scss --routing
 ```
+# Generate components
+```bash
+ng g c register
+ng g c navbar
+ng g c quiz
+ng g c result
+ng g c shared
+```
+# Generate quize service
+```bash
+ng g s shared/quiz
+```
+# Install [materialize](https://materializecss.com/getting-started.html)
+```bash
+
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ResultComponent } from './result/result.component';
+import { SharedComponent } from './shared/shared.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    NavbarComponent,
+    QuizComponent,
+    ResultComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
